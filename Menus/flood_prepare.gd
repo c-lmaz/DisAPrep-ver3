1914d489a221d5e6cb7320b3ecafc8e40c4c1e43
c-lmaz <lailamas.ahmadz@gmail.com>
2024-09-15 19:19:11 +0800

    Kit items placed

--- Menus/flood_prepare.gd
+++ Menus/flood_prepare.gd
@@ -4,10 +4,11 @@ extends CanvasLayer
 @onready var texture_rect = $TextureRect
 
 @onready var hud = $HUD
-
-var subv_snapshot
-var subv_tex
 var level_phase = ["Flooood", "Prep"]
+
+@onready var kit_items = $KitItems
+var rooms = ["Outside", "Living", "Kitchen", "Bathroom", "Family", "Bedroom"]
+var room_ind = 1
 
 func _ready():
 	hud.set_level_name(level_phase)
@@ -21,8 +22,18 @@ func _process(_delta):
 			camera.position.x = 1.5
 			camera.position.z = 3.1
 			camera.rotation = Vector3(0,0,0)
-		elif camera.position.x < 13.5: camera.position.x += 3
-		else: camera.position.x = 13.5
+			room_ind = 1
+			kit_items.show_room(rooms[room_ind])
+		
+		elif camera.position.x < 13.5: 
+			camera.position.x += 3
+			room_ind += 1
+			kit_items.show_room(rooms[room_ind])
+		
+		else: 
+			camera.position.x = 13.5
+			room_ind = 5
+			kit_items.show_room(rooms[room_ind])
 		
 	
 	if Input.is_action_just_pressed("ui_left"):
@@ -30,7 +41,13 @@ func _process(_delta):
 			camera.position.x = -1.9
 			camera.position.z = 1.5
 			camera.rotation = Vector3(0,PI/-2,0)
-		else: camera.position.x -= 3
+			room_ind = 0
+			kit_items.show_room(rooms[room_ind])
+		
+		else: 
+			camera.position.x -= 3
+			room_ind -= 1
+			kit_items.show_room(rooms[room_ind])
 
 
 func _on_hud_game_paused(pause_state):
---
+++ Menus/kit_items.gd
@@ -0,0 +1,50 @@
+extends Control
+
+@onready var kit_menu = $KitMenu
+@onready var kit_menu_cont = $KitMenu/VBoxContainer
+
+var current_room : String
+var current_spot : String
+
+func _ready():
+	for control in get_children():
+		for button in control.get_children():
+			if button is TextureButton:
+				button.connect("pressed", _on_button_pressed.bind(button, control))
+
+
+func _on_button_pressed(button, parent):
+	current_spot = button.name
+	current_room = parent.name
+	for menu in kit_menu_cont.get_children():
+		if menu.name == current_spot:
+			menu.visible = true
+			kit_menu.visible = true
+			parent.visible = false
+
+
+func show_room(room: String):
+	for control in get_children():
+		if control.name == room and control.visible == false:
+			control.visible = true
+		else:
+			control.visible = false
+
+
+func _on_collect_button_pressed():
+	var node = kit_menu_cont.get_node(current_spot)
+	# TODO: validate choices and give score
+	# +5 if correct, -1 if wrong
+	for item in node.get_children():
+		if item.button_pressed:
+			print(item.name)
+	
+	# Toggle visiblity
+	var room = get_node(current_room)
+	room.visible = true
+	kit_menu.visible = false
+	
+	# Handle queuefree
+	var node_button = room.get_node(current_spot)
+	node.queue_free()
+	node_button.queue_free()
