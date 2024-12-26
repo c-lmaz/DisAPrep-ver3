6b61c6518117155b71349948440af98106e2c1f4
c-lmaz <lailamas.ahmadz@gmail.com>
2024-12-26 22:19:56 +0800

    Finished notes

--- Menus/notes_menu.gd
+++ Menus/notes_menu.gd
@@ -1,21 +1,29 @@
 extends Panel
 
-@onready var _999n = $"ScrollContainer/VBoxContainer/999N"
-@onready var info_source_n = $ScrollContainer/VBoxContainer/InfoSourceN
-@onready var emergency_kit_n = $ScrollContainer/VBoxContainer/EmergencyKitN
-@onready var first_aid_n = $ScrollContainer/VBoxContainer/FirstAidN
-
-@onready var drsn = $ScrollContainer/VBoxContainer/FirstAidN/DRSN
-@onready var avpun = $ScrollContainer/VBoxContainer/FirstAidN/AVPUN
-@onready var wounds_n = $ScrollContainer/VBoxContainer/FirstAidN/WoundsN
-@onready var sprain_n = $ScrollContainer/VBoxContainer/FirstAidN/SprainN
+@onready var scroll_container = $ScrollContainer
 
 
 func _ready():
 	for button in $ScrollContainer/VBoxContainer.get_children():
 		if button is Button:
-			button.connect("toggled", _on_button_toggled.bind(button.name))
+			button.connect("toggled", _on_button_toggled.bind(button))
+	
+	for button in $ScrollContainer/VBoxContainer/FirstAidN.get_children():
+		if button is Button:
+			button.connect("toggled", _on_button_toggled.bind(button))
 
 
-func _on_button_toggled(toggle: bool, b_name: String):
-	pass
+func _on_button_toggled(toggled_on: bool, button: Button):
+	var b_name = button.name
+	var note
+	match b_name:
+		"999": note = $"ScrollContainer/VBoxContainer/999N"
+		"InfoSource": note = $ScrollContainer/VBoxContainer/InfoSourceN
+		"EmergencyKit": note = $ScrollContainer/VBoxContainer/EmergencyKitN
+		"FirstAid": note = $ScrollContainer/VBoxContainer/FirstAidN
+		"DRS": note = $ScrollContainer/VBoxContainer/FirstAidN/DRSN
+		"AVPU": note = $ScrollContainer/VBoxContainer/FirstAidN/AVPUN
+		"Wounds": note = $ScrollContainer/VBoxContainer/FirstAidN/WoundsN
+		"Sprain": note = $ScrollContainer/VBoxContainer/FirstAidN/SprainN
+	
+	note.visible = toggled_on
